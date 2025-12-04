6385dcbe317fbf2ddc54e4ca39887d6b793d9802
aaron-iz <aron.iz.gab@gmail.com>
2025-12-04 14:20:28 +0200

    some formatting issues + hiccups

--- src/Chronos.MainApi/Auth/Contracts/CreateUserRequest.cs
+++ src/Chronos.MainApi/Auth/Contracts/CreateUserRequest.cs
@@ -1,3 +1,3 @@
 ﻿namespace Chronos.MainApi.Auth.Contracts;
 
-public record CreateUserRequest(string Email, string FirstName, string LastName);
+public record CreateUserRequest(string Email, string FirstName, string LastName, string Password);
