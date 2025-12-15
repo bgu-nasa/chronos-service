52bdbe9e481864bb08c372fdb4690bcc8b3eff25
aaron-iz <aron.iz.gab@gmail.com>
2025-12-15 19:28:17 +0200

    config to db

---
+++ src/Chronos.Data/ModelConfig/DepartmentConfiguration.cs
@@ -0,0 +1,33 @@
+using Chronos.Data.Utils;
+using Chronos.Domain.Management;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Chronos.Data.ModelConfig;
+
+public class DepartmentConfiguration : IEntityTypeConfiguration<Department>
+{
+    public void Configure(EntityTypeBuilder<Department> builder)
+    {
+        builder.ToTable(ConfigUtils.ToTableName(nameof(Department)));
+
+        // Key config:
+        builder.HasKey(d => d.Id);
+
+        // Property config:
+        builder.Property(d => d.OrganizationId)
+            .IsRequired();
+
+        builder.Property(d => d.Name)
+            .IsRequired()
+            .HasMaxLength(256);
+
+        builder.Property(d => d.Deleted)
+            .HasDefaultValue(false);
+
+        builder.Property(d => d.DeletedTime);
+
+        builder.Property(d => d.CreatedAt);
+        builder.Property(d => d.UpdatedAt);
+    }
+}
