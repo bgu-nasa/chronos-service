247403bc440dac7e154a57d87c1ca71aec124ceb
aaron-iz <aron.iz.gab@gmail.com>
2025-12-15 22:46:25 +0200

    Create test projects empty of tests for now

---
+++ tests/Chronos.Tests.MainApi/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Chronos.Tests.MainApi;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
