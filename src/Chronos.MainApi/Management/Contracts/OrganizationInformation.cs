bd1c1786f623aad890e2a171e3f73862edec36df
Aaron Iziyaev <128286172+aaron-iz@users.noreply.github.com>
2025-12-18 00:02:49 +0200

    [Management] Organization web api (#31)
    * define some contracts
    
    * added soft delete api for org
    
    * small change I forgot
    
    * Add role mapper
    
    * fix I got confused with other file in previous commits
    
    * add `OrganizationInfoService`
    
    * add info api
    
    * forgot to add auhtorize
    
    * Pass deletion in the contract for the client

--- src/Chronos.MainApi/Management/Contracts/OrganizationInformation.cs
+++ src/Chronos.MainApi/Management/Contracts/OrganizationInformation.cs
@@ -3,4 +3,11 @@ namespace Chronos.MainApi.Management.Contracts;
 /// <summary>
 /// Large object which summarizes all information about an organization that is needed in another service.
 /// </summary>
-public record OrganizationInformation();
+public record OrganizationInformation(
+    Guid Id,
+    string Name,
+    bool Deleted,
+    DateTime DeletedTime,
+    RoleAssignmentResponse[] UserRoles,
+    DepartmentResponse[] Departments
+);
--- src/Chronos.MainApi/Management/Controllers/OrganizationController.cs
+++ src/Chronos.MainApi/Management/Controllers/OrganizationController.cs
@@ -1,8 +1,101 @@
+using System.Security.Claims;
+using Chronos.MainApi.Management.Services;
+using Chronos.MainApi.Shared.Middleware;
+using Chronos.Shared.Exceptions;
+using Chronos.Shared.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Chronos.MainApi.Management.Controllers;
 
-public class OrganizationController
+[ApiController]
+[Route("api/management/[controller]")]
+public class OrganizationController(
+    ILogger<OrganizationController> logger,
+    IOrganizationService organizationService,
+    IOrganizationInfoService infoService)
+    : ControllerBase
 {
-    // ONLY:
-    // Will have the DepartmentInformation GET endpoint, nothing else for now.
-    // Will have the DELETE organization endpoint & undelete.
+    [Authorize]
+    [HttpGet("/info")]
+    public async Task<IActionResult> GetOrganizationInfoAsync()
+    {
+        logger.LogInformation("Get organization info");
+
+        var organizationId = GetOrganizationIdFromContext();
+        var userIdClaim = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier); // TODO move to extension
+        if (userIdClaim is null)
+        {
+            logger.LogError("No user id claim found in HttpContext, when expecting one");
+            throw new UnauthorizedAccessException();
+        }
+
+        var isValidUserId = Guid.TryParse(userIdClaim.Value, out var userId);
+        if (!isValidUserId)
+        {
+            logger.LogError("Invalid user id in claims: {UserId}", userIdClaim.Value);
+            throw new UnauthorizedAccessException();
+        }
+
+        var orgInfo = await infoService.GetOrganizationInformationAsync(Guid.Parse(organizationId), userId);
+
+        return Ok(orgInfo);
+    }
+
+    [RequireOrganization]
+    [Authorize(Policy = "OrgRole:Administrator")]
+    [HttpDelete]
+    public async Task<IActionResult> DeleteOrganization()
+    {
+        logger.LogInformation("Soft delete organization endpoint was called");
+
+        var organizationId = GetOrganizationIdFromContext();
+
+        try
+        {
+            await organizationService.SetForDeletionAsync(Guid.Parse(organizationId));
+
+            return NoContent();
+        }
+        catch (FormatException e)
+        {
+            logger.LogInformation("The organization ID provided is not in a valid format: {OrganizationId}", organizationId);
+            throw new BadRequestException("The organization ID is not in a valid format.", e);
+        }
+    }
+
+    [RequireOrganization]
+    [Authorize(Policy = "OrgRole:Administrator")]
+    [HttpPost("/restore")]
+    public async Task<IActionResult> RestoreOrganization()
+    {
+        logger.LogInformation("Restore organization endpoint was called");
+
+        var organizationId = GetOrganizationIdFromContext();
+
+        try
+        {
+            await organizationService.RestoreDeletedOrganizationAsync(Guid.Parse(organizationId));
+
+            return NoContent();
+        }
+        catch (FormatException e)
+        {
+            logger.LogInformation("The organization ID provided is not in a valid format: {OrganizationId}", organizationId);
+            throw new BadRequestException("The organization ID is not in a valid format.", e);
+        }
+    }
+
+    private string GetOrganizationIdFromContext()
+    {
+        var organizationId = HttpContext.GetOrganizationId();
+
+        if (organizationId is null)
+        {
+            logger.LogCritical("No organization id was found in the HttpContext, although infra policy requires so.");
+            throw new BadRequestException("Missing organization ID in request.");
+        }
+
+        return organizationId;
+    }
 }
