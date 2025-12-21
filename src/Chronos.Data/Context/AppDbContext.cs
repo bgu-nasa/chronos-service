2fe995ca91896acdc991bcb7d00ad548d50d4715
ShalevKk <kayatsha@post.bgu.ac.il>
2025-12-21 20:39:32 +0200

    Added Resource repositories to DbContext

--- src/Chronos.Data/Context/AppDbContext.cs
+++ src/Chronos.Data/Context/AppDbContext.cs
@@ -4,6 +4,7 @@ using Chronos.Domain.Management;
 using Chronos.Domain.Management.Roles;
 using Chronos.Shared.Extensions;
 using Microsoft.AspNetCore.Http;
+using Chronos.Domain.Resources;
 using Microsoft.EntityFrameworkCore;
 
 namespace Chronos.Data.Context;
@@ -20,6 +21,14 @@ public class AppDbContext(DbContextOptions<AppDbContext> options, IHttpContextAc
     public DbSet<Organization> Organizations => Set<Organization>();
     public DbSet<Department> Departments => Set<Department>();
     public DbSet<RoleAssignment> RoleAssignments => Set<RoleAssignment>();
+    
+    // Resources
+    public DbSet<Subject> Subjects => Set<Subject>();
+    public DbSet<Activity> Activities => Set<Activity>();
+    public DbSet<Resource> Resources => Set<Resource>();
+    public DbSet<ResourceAttribute> ResourceAttributes => Set<ResourceAttribute>();
+    public DbSet<ResourceType> ResourceTypes => Set<ResourceType>();
+    public DbSet<ResourceAttributeAssignment> ResourceAttributeAssignments => Set<ResourceAttributeAssignment>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -36,6 +45,13 @@ public class AppDbContext(DbContextOptions<AppDbContext> options, IHttpContextAc
             modelBuilder.Entity<Organization>().HasQueryFilter(o => o.Id.ToString().ToLower() == _currentOrganizationId);
             modelBuilder.Entity<Department>().HasQueryFilter(d => d.OrganizationId.ToString().ToLower() == _currentOrganizationId);
             modelBuilder.Entity<RoleAssignment>().HasQueryFilter(ra => ra.OrganizationId.ToString().ToLower() == _currentOrganizationId);
+            
+            // Resources
+            modelBuilder.Entity<Subject>().HasQueryFilter(s => s.OrganizationId.ToString().ToLower() == _currentOrganizationId);
+            modelBuilder.Entity<Activity>().HasQueryFilter(a => a.OrganizationId.ToString().ToLower() == _currentOrganizationId);
+            modelBuilder.Entity<Resource>().HasQueryFilter(r => r.OrganizationId.ToString().ToLower() == _currentOrganizationId);
+            modelBuilder.Entity<ResourceAttribute>().HasQueryFilter(ra => ra.OrganizationId.ToString().ToLower() == _currentOrganizationId);
+            modelBuilder.Entity<ResourceType>().HasQueryFilter(rt => rt.OrganizationId.ToString().ToLower() == _currentOrganizationId);
         }
 
     }
