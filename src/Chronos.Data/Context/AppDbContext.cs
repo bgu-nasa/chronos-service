e07b21687a1944af1b53fcc3d0d058a54f2cc777
ShalevKk <kayatsha@post.bgu.ac.il>
2025-12-25 12:45:13 +0200

    Added organizationId to Subject and Activity and fixed their configuration; Added Subject and Activity to db context

--- src/Chronos.Data/Context/AppDbContext.cs
+++ src/Chronos.Data/Context/AppDbContext.cs
@@ -1,41 +1,45 @@
-﻿using System.Reflection;
+﻿﻿using System.Reflection;
 using Chronos.Domain.Auth;
 using Chronos.Domain.Management;
 using Chronos.Domain.Management.Roles;
 using Chronos.Shared.Extensions;
 using Microsoft.AspNetCore.Http;
+using Chronos.Domain.Resources;
 using Microsoft.EntityFrameworkCore;
 
 namespace Chronos.Data.Context;
-
 public class AppDbContext(DbContextOptions<AppDbContext> options, IHttpContextAccessor httpContextAccessor)
     : DbContext(options)
 {
     private readonly string? _currentOrganizationId = httpContextAccessor.HttpContext.GetOrganizationId()?.ToLower();
-
     // Auth
     public DbSet<User> Users => Set<User>();
-
     // Management
     public DbSet<Organization> Organizations => Set<Organization>();
     public DbSet<Department> Departments => Set<Department>();
     public DbSet<RoleAssignment> RoleAssignments => Set<RoleAssignment>();
 
+    // Resources
+    public DbSet<Subject> Subjects => Set<Subject>();
+    public DbSet<Activity> Activities => Set<Activity>();
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
-
         // Add your entities here to make sure no leaks between orgs (tenants)
         if (_currentOrganizationId is not null)
         {
             // Auth
             modelBuilder.Entity<User>().HasQueryFilter(u => u.OrganizationId.ToString().ToLower() == _currentOrganizationId);
-
             // Management
             modelBuilder.Entity<Organization>().HasQueryFilter(o => o.Id.ToString().ToLower() == _currentOrganizationId);
             modelBuilder.Entity<Department>().HasQueryFilter(d => d.OrganizationId.ToString().ToLower() == _currentOrganizationId);
             modelBuilder.Entity<RoleAssignment>().HasQueryFilter(ra => ra.OrganizationId.ToString().ToLower() == _currentOrganizationId);
+
+            // Resources
+            modelBuilder.Entity<Subject>().HasQueryFilter(s => s.OrganizationId.ToString().ToLower() == _currentOrganizationId);
+            modelBuilder.Entity<Activity>().HasQueryFilter(a => a.OrganizationId.ToString().ToLower() == _currentOrganizationId);
         }
 
     }
--- src/Chronos.Data/ModelConfig/Resources/SubjectConfiguration.cs
+++ src/Chronos.Data/ModelConfig/Resources/SubjectConfiguration.cs
@@ -2,9 +2,7 @@ using Chronos.Data.Utils;
 using Chronos.Domain.Resources;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-
 namespace Chronos.Data.ModelConfig.Resources;
-
 public class SubjectConfiguration : IEntityTypeConfiguration<Subject>
 {
     public void Configure(EntityTypeBuilder<Subject> builder)
@@ -13,22 +11,22 @@ public class SubjectConfiguration : IEntityTypeConfiguration<Subject>
 
         builder.HasKey(s => s.Id);
 
+        builder.Property(s => s.OrganizationId)
+            .IsRequired();
+
         builder.Property(s => s.DepartmentId)
             .IsRequired();
-
         builder.Property(s => s.SchedulingPeriodId)
             .IsRequired();
-
         builder.Property(s => s.Code)
             .IsRequired()
             .HasMaxLength(128);
-
         builder.Property(s => s.Name)
             .IsRequired()
             .HasMaxLength(256);
 
-        builder.HasIndex(s => new { s.DepartmentId, s.Code })
+        builder.HasIndex(s => new { s.DepartmentId, s.Code });
+        builder.HasIndex(s => new { s.OrganizationId, s.DepartmentId, s.Code })
             .IsUnique();
     }
 }
-
