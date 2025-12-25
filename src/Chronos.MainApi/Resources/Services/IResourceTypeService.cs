f68451a9a00ecda46fb5204383905a358d279032
ShalevKk <kayatsha@post.bgu.ac.il>
2025-12-26 01:54:48 +0200

    Fixed a bug: created Resource entities' services as repositories. Now implemented properly

---
+++ src/Chronos.MainApi/Resources/Services/IResourceTypeService.cs
@@ -0,0 +1,12 @@
+using Chronos.Domain.Resources;
+
+namespace Chronos.MainApi.Resources.Services;
+
+public interface IResourceTypeService
+{
+    Task<Guid> CreateResourceTypeAsync(Guid organizationId, string type);
+    Task<ResourceType> GetResourceTypeAsync(Guid organizationId, Guid resourceTypeId);
+    Task<List<ResourceType>> GetResourceTypesAsync(Guid organizationId);
+    Task UpdateResourceTypeAsync(Guid organizationId, Guid resourceTypeId, string type);
+    Task DeleteResourceTypeAsync(Guid organizationId, Guid resourceTypeId);
+}
