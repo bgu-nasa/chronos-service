60e52241b3e0e03b5fd2a4ceeefa78755d997c62
aaron-iz <aron.iz.gab@gmail.com>
2026-01-06 21:21:08 +0200

    configure json globally

--- src/Chronos.MainApi/Program.cs
+++ src/Chronos.MainApi/Program.cs
@@ -64,7 +64,11 @@ builder.Services.AddSingleton<IAuthorizationHandler, RequireRoleOrgHandler>();
 builder.Services.AddSingleton<IAuthorizationHandler, RequireRoleDeptHandler>();
 
 // Add services to the container
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+    });
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
