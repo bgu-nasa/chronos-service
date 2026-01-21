77ec8f9c99a2b15b8d22d2c0495c12900ed1b16b
Aaron Iziyaev <128286172+aaron-iz@users.noreply.github.com>
2026-01-21 22:46:41 +0200

    [Infra-Auth] Pre deployment, do not allow infinite amount of organizations via invite code (#120)
    * create hacky service for this
    
    * add this to the api
    
    * add test

---
+++ src/Chronos.MainApi/Auth/Services/HackyInvitationService.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace Chronos.MainApi.Auth.Services;
+
+// TODO Remove this bullshit
+public class HackyInvitationService
+{
+    private const long Prime = 22697L;
+    private const long TopMultiplier = 10_000L;
+    private const string Map = "0123456789abcdefghijklmnopqrstuvwxyz";
+
+    private readonly Random _random = new();
+
+    public bool VerifyInviteCode(string code)
+    {
+        var num = 0L;
+        var arr = code.ToCharArray().Reverse().ToArray();
+
+        foreach (var c in arr)
+        {
+            num *= Map.Length;
+            num += Map.IndexOf(c);
+        }
+
+        return num % Prime == 0;
+    }
+
+    public string GenerateInviteCode()
+    {
+        var builder = new StringBuilder();
+        var multiplier = _random.NextInt64(TopMultiplier);
+        var num = multiplier * Prime;
+        var radix = Map.Length;
+
+        while (num > 0)
+        {
+            builder.Append(Map[(int)num % radix]);
+            num /= radix;
+        }
+
+        return builder.ToString();
+    }
+}
