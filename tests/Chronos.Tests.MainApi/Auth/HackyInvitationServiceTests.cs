26405378538b1faa0ed792c8913b50f80a47a6ca
Aaron Iziyaev <aron.iz.gab@gmail.com>
2026-01-21 20:53:22 +0200

    add test

---
+++ tests/Chronos.Tests.MainApi/Auth/HackyInvitationServiceTests.cs
@@ -0,0 +1,15 @@
+﻿using Chronos.MainApi.Auth.Services;
+
+namespace Chronos.Tests.MainApi.Auth;
+
+[TestFixture]
+public class HackyInvitationServiceTests
+{
+    [Test]
+    public void ValidateGeneratedCode_ShouldReturnTrue()
+    {
+        var service = new HackyInvitationService();
+        var code = service.GenerateInviteCode();
+        Assert.That(service.VerifyInviteCode(code), Is.True);
+    }
+}
